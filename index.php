675e494ba57c097b1f222c8caa88eb53e449d2d0
Oleksandr <op@jelastic.com>
2019-03-15 09:37:26 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php
+echo '<p>Hello World</p>'; ?> 
+ <br/>
+ <?php
+
+// Show all information, defaults to INFO_ALL
+
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+
+phpinfo(INFO_MODULES);
+?>
+ </body>
+</html>
